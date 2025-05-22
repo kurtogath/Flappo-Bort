baad1014adfdbc0ae6df2b21066a1b44ef4e4a6d
Oscar <kurtogath@gmail.com>
2025-05-22 19:26:25 +0200

    Primer commit del clon de Flappy Bird en Unity

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdsIsAlive = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true && birdsIsAlive)
+        {
+            myRigidBody.linearVelocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdsIsAlive = false;
+    }
+}
