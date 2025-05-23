cbc1c8c3d2faf0bd50dfa9d22873deaa31e21cd2
Oscar Marti <oscarmartitech@gmail.com>
2025-05-23 15:35:06 +0200

    Added update skin

--- Assets/Scripts/ConfigManager.cs
+++ Assets/Scripts/ConfigManager.cs
@@ -4,15 +4,20 @@ using TMPro;
 
 public class ConfigManager : MonoBehaviour
 {
+    public TMP_Text selectedSkinText;
     public TMP_InputField nameInput;
     public TMP_Text currentNameText;
 
     void Start()
     {
         string nombreGuardado = PlayerPrefs.GetString("PlayerName", "Invitado");
-
+        
+        
         if (nameInput != null)
             nameInput.text = nombreGuardado;
+
+        currentNameText.text = nombreGuardado;
+        UpdateSkinLabel();
 
     }
 
@@ -39,7 +44,15 @@ public class ConfigManager : MonoBehaviour
     {
         PlayerPrefs.SetInt("BirdSkin", skinIndex);
         PlayerPrefs.Save();
-        Debug.Log("Skin guardada: " + skinIndex);
+        UpdateSkinLabel();
+    }
+
+    public void UpdateSkinLabel()
+    {
+        int skinIndex = PlayerPrefs.GetInt("BirdSkin", 0);
+        string skinName = skinIndex == 0 ? "Pájaro" : "DarioxGD";
+        selectedSkinText.text = $"Skin: {skinName}";
+        Debug.Log("Skin: " + skinIndex);
     }
 
 }
